c6f756bf159c3313991221bddf5f9d1458fbe424
mestrequ <mestre.quentin0@gmail.com>
2018-04-06 15:57:36 +0200

    sass comin

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
@@ -25,9 +26,9 @@
           <li class="nav-item text-secondary"><a class="nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>">Fonctionnalités</a></li>
           <li class="nav-item text-dark"><a class="nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><strong>Exemples</strong></a></li>
           <li class="nav-item text-secondary"><a class="nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>">Tarifs</a></li>
+          <li class="nav-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" role="button" class="btn btn-outline-primary" style="margin-left : 10px; border-radius : 40px">Connexion</a></li>
+          <li class="nav-item"><a href=" <?php echo esc_url( home_url( '/' ) ); ?>" role="button" class="btn btn-primary" style="margin-left : 10px; border-radius : 40px">Créer un compte <span class="badge badge-light">BÊTA</span></a></li>
         </ul>
-        <a class="nav-link btn btn-outline-primary justify-content-end" href="<?php echo esc_url( home_url( '/' ) ); ?>">Connexion</a>
-        <a class="nav-link btn btn-primary justify-content-end" href=" <?php echo esc_url( home_url( '/' ) ); ?>">Créer un compte <span class="badge badge-light">BÊTA</span></a>
       </div>
     </nav>
     <div class="container-fluid" style="width : 100%; padding-right : 0px; padding-left : 0px; margin-bottom: 50px;">
